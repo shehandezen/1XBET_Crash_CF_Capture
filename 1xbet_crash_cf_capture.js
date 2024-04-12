65599b56d1734a570c7c24e79cf89c8723df55ab
shehandezen <shehandezen@gmail.com>
2024-04-12 12:39:32 +0100

    implemented web service

--- 1xbet_crash_cf_capture.js
+++ 1xbet_crash_cf_capture.js
@@ -5,48 +5,77 @@ const wait = (ms) => new Promise(res => setTimeout(res, ms));
 
 //get WsEndpoint
 async function getWsEndpoint() {
-    let wsEndpoint = await launch();
-    return wsEndpoint;
+  let wsEndpoint = await launch();
+  return wsEndpoint;
 }
-
+let x
 (async () => {
-    const browser = await puppeteer.connect({
-        browserWSEndpoint: await getWsEndpoint(),
-        defaultViewport: null,
-    });
+  const browser = await puppeteer.connect({
+    browserWSEndpoint: await getWsEndpoint(),
+    defaultViewport: null,
+  });
 
-    let page = await browser.newPage();
-    await page.goto("https://1xbet.com/en/allgamesentrance/crash");
+  let page = await browser.newPage();
+  await page.goto("https://1xbet.com/en/allgamesentrance/crash", { timeout: 60000 });
+  await page.setViewport({
+    width: 900,
+    height: 800,
+    isMobile: false,
+    isLandscape: true,
+    hasTouch: false,
+    deviceScaleFactor: 1
+  })
 
-    const client = await page.target().createCDPSession()
+  const client = await page.target().createCDPSession()
 
-    await client.send('Network.enable')
+  await client.send('Network.enable')
 
-    client.on('Network.webSocketFrameReceived', ({ requestId, timestamp, response }) => {
-        let payloadString = response.payloadData.toString('utf8');
+  client.on('Network.webSocketFrameReceived', ({ requestId, timestamp, response }) => {
+    let payloadString = response.payloadData.toString('utf8');
+   
+
+    try {
+
+      if (payloadString.includes('"target":"OnStart"')) {
+
+        payloadString = payloadString.replace(/[^\x20-\x7E]/g, '');
+        const payload = JSON.parse(payloadString);
+        console.log(payload)
+        fs.appendFile('data.txt', payload, (err) => {
+          if (err) throw err;
+        });
+        const { ts } = payload.arguments[0];
+        x = ts
+      }
+      if (payloadString.includes('"type":1,"target":"OnCrash"')) {
         
-        try {
-          if (payloadString.includes('"ic":true')) {
-            payloadString = payloadString.replace(/[^\x20-\x7E]/g, '');
-            const payload = JSON.parse(payloadString);
-      
-            const { cf, mfs, ts } = payload.arguments[0];
-            console.log(cf, mfs, ts);
-            const csvData = `${cf},${mfs},${ts}\n`;
-            
-            fs.appendFile('data.csv', csvData, (err) => {
-              if (err) throw err;
-            });
-          }
-        } catch (error) {
-          console.error('Error processing WebSocket frame:', error);
-        }
-      });
+        payloadString = payloadString.replace(/[^\x20-\x7E]/g, '');
+        const payload = JSON.parse(payloadString);
+        fs.appendFile('data.txt', payload, (err) => {
+          if (err) throw err;
+        });
+        const { f, ts } = payload.arguments[0];
+        console.log(`${timestamp} >> ${f}, ${x}, ${ts}`);
+        const csvData = `${f},${x},${ts}\n`;
 
-    while(true){
-        await page.keyboard.press("Tab");
-        await wait(1000);
-        await page.keyboard.press("ArrowDown");
-        await wait(1000);
+        fs.appendFile('data.csv', csvData, (err) => {
+          if (err) throw err;
+        });
+      }
+    } catch (error) {
+      console.error('Error processing WebSocket frame:', error);
     }
+  });
+
+  while (true) {
+    await page.keyboard.press("Tab");
+    await wait(1000);
+
+    await wait(1000);
+  }
 })();
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,81 @@
+const puppeteer = require("puppeteer-extra");
+const launch = require("./launch");
+const fs = require('fs');
+const wait = (ms) => new Promise(res => setTimeout(res, ms));
+
+const app = require('express')()
+
+const PORT = 3000
+//get WsEndpoint
+async function getWsEndpoint() {
+    let wsEndpoint = await launch();
+    return wsEndpoint;
+}
+let x
+(async () => {
+    const browser = await puppeteer.connect({
+        browserWSEndpoint: await getWsEndpoint(),
+        defaultViewport: null,
+    });
+
+    let page = await browser.newPage();
+    await page.goto("https://1xbet.com/en/allgamesentrance/crash", { timeout: 90000 });
+    await page.setViewport({
+        isMobile: false,
+        isLandscape: true,
+        hasTouch: false,
+        deviceScaleFactor: 1
+    })
+
+    const client = await page.target().createCDPSession()
+
+    await client.send('Network.enable')
+
+    client.on('Network.webSocketFrameReceived', ({ requestId, timestamp, response }) => {
+        let payloadString = response.payloadData.toString('utf8');
+
+
+        try {
+
+            if (payloadString.includes('"target":"OnStart"')) {
+
+                payloadString = payloadString.replace(/[^\x20-\x7E]/g, '');
+                const payload = JSON.parse(payloadString);
+                console.log(payload)
+                fs.appendFile('data.txt', toString(payload), (err) => {
+                    if (err) throw err;
+                });
+                const { ts } = payload.arguments[0];
+                x = ts
+            }
+            if (payloadString.includes('"type":1,"target":"OnCrash"')) {
+
+                payloadString = payloadString.replace(/[^\x20-\x7E]/g, '');
+                const payload = JSON.parse(payloadString);
+                fs.appendFile('data.txt', toString(payload), (err) => {
+                    if (err) throw err;
+                });
+                const { f, ts } = payload.arguments[0];
+                console.log(`${timestamp} >> ${f}, ${x}, ${ts}`);
+                const csvData = `${f},${x},${ts}\n`;
+
+                fs.appendFile('data.csv', csvData, (err) => {
+                    if (err) throw err;
+                });
+            }
+        } catch (error) {
+            console.error('Error processing WebSocket frame:', error);
+        }
+    });
+
+    while (true) {
+        await page.keyboard.press("Tab");
+        await wait(2000);
+    }
+})();
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
